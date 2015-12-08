4e85cbad0ccc9f3a6f2fda6afd7897f919c0850b
Amol Rokade <amol.woxi@gmail.com>
2015-12-08 15:22:22 +0530

    Migration added

---
+++ database/migrations/2015_12_08_075405_create_event_type_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventTypeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('event_name',255);
+            $table->string('slug',255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('event_types');
+    }
+}
