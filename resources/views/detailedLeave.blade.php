af2a44d316642107be17c6ec7c03cbffae0c6684
Suraj Bande <suraj.woxi@gmail.com>
2015-12-10 16:41:57 +0530

    dtailed leave type

--- resources/views/detailedLeave.blade.php
+++ resources/views/detailedLeave.blade.php
@@ -67,6 +67,8 @@
                                                 <span class="text-bold">Leave From:</span> 12 Nov, 2015 12:00 PM
                                                 <br>
                                                 <span class="text-bold">Leave To:</span> 14 Nov, 2015 6:00 PM
+                                                <br>
+                                                <span class="text-bold">Leave Type:</span> Full Day Leave
                                             </address>
 
                                             <div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 3px;"><div class="ps-scrollbar-x" style="left: 0px; width: 0px;"></div></div><div class="ps-scrollbar-y-rail" style="top: 0px; height: 180px; right: 3px;"><div class="ps-scrollbar-y" style="top: 0px; height: 82px;"></div></div></div>
