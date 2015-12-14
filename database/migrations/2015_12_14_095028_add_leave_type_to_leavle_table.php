dd53cde9d47816aa7f9e833bb537d8c7f2030b83
Amol Rokade <amol.woxi@gmail.com>
2015-12-14 15:22:28 +0530

    migrations

---
+++ database/migrations/2015_12_14_095028_add_leave_type_to_leavle_table.php
@@ -0,0 +1,32 @@
+<?php
+
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Database\Migrations\Migration;
+
+class AddLeaveTypeToLeaveField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('leaves', function (Blueprint $table) {
+            $table->string('leave_type',255)
+                ->after('title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('leaves', function (Blueprint $table) {
+            $table->dropColumn('leave_type');
+        });
+    }
+}
