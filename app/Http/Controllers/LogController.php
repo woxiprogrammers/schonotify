e6b8e54e7dddc355fac84205542273efdb2f0862
Suraj Bande <suraj.woxi@gmail.com>
2015-12-15 16:08:38 +0530

    auth added in logcontroller

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -27,6 +27,7 @@ class LogController extends Controller
     public function __construct()
     {
         $this->middleware('db');
+        $this->middleware('auth',['only'=>'logout']);
     }
 
     public function index()
