8c8a53561f4c4fd03fda84a9271efdbe6c9c1adf
Amol Rokade <amol.woxi@gmail.com>
2015-12-29 18:25:57 +0530

    done

--- app/Http/Controllers/api/ResultController.php
+++ app/Http/Controllers/api/ResultController.php
@@ -20,20 +20,50 @@ class ResultController extends Controller
      */
     public function viewResult(Requests\Result $request , $id)
     {
-       // $user=User::where('id','=',$id)->first();
         $studentResult=Result::where('student_id','=',$id)->get();
         $studentResultArray=$studentResult->toArray();
         $i=0;
         foreach($studentResultArray as $value){
             $exam_subject=ExamSubjects::where('id','=',$value['exam_subject_id'])->first();
-            dd($exam_subject);
-            $examName=Exams::where('id','=',$exam_subject['exam_id'])->first();
-            $data[$i]['exam_name']=$examName['exam_name'];
+            $exam=Exams::where('id','=',$exam_subject['exam_id'])->first();
+            $data[$i]['exam_name']=$exam['exam_name'];
             $data[$i]['marks']=$value['student_marks'];
-            $subjectName=Subject::where('id','=',$exam_subject['subject_id'])->first();
-            $data[$i]['subject']=$subjectName['subject_name'];
+            $subject=Subject::where('id','=',$exam_subject['subject_id'])->first();
+            $data[$i]['subject']=$subject['subject_name'];
                 $i++;
         }
-        dd($data);
+        $size=count($data);
+        $finalExamArray = array();
+        $examResultArray = $data;
+        $examName="";
+        for($i=0,$j=0;$i<$size;$i++){
+            if($examName==$examResultArray[$i]['exam_name']){
+                $j++;
+            }else{
+                $j=0;
+            }
+            $result = array(
+                'marks'=> $examResultArray[$i]['marks'],
+                'subject' => $examResultArray[$i]['subject'] );
+            $examName=$examResultArray[$i]['exam_name'];
+            $finalExamArray[$examName][$j] = $result;
+        }
+        $size=count($data);
+        $finaleSubjectArray = array();
+        $subjectName="";
+        $subjectResultArray = $data;
+        for($i=0,$j=0;$i<$size;$i++){
+            if($subjectName==$subjectResultArray[$i]['subject']){
+                $j++;
+            }else{
+                $j=0;
+            }
+            $result = array(
+                'exam_name'=> $examResultArray[$i]['exam_name'],
+                'marks' => $examResultArray[$i]['marks'] );
+            $subjectName=$examResultArray[$i]['subject'];
+            $finaleSubjectArray[$subjectName][$j] = $result;
+        }
+        return $finaleSubjectArray;
     }
 }
