8a9aea96fd792027f526bbd77ed383c07037226d
ganesh.woxi@gmail.com <ganesh.woxi@gmail.com>
2015-12-31 12:59:32 +0530

    front controller chaneges done

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -27,19 +27,18 @@ class FrontController extends Controller
     {
         if(isset(Auth::user()->email))
         {
-            $val1= User::Join('module_acls', 'users.id', '=', 'module_acls.user_id')
+            $userInfo= User::Join('module_acls', 'users.id', '=', 'module_acls.user_id')
                 ->Join('acl_master', 'module_acls.acl_id', '=', 'acl_master.id')
                 ->Join('modules', 'modules.id', '=', 'module_acls.module_id')
                 ->where('users.id','=',Auth::user()->id)
                 ->select('users.id','users.email','users.username as username','users.first_name as firstname','users.last_name as lastname','acl_master.slug as acl','modules.title as module','modules.slug as module_slug')
                 ->get();
-            $i=0;
             $resultArr=array();
-            foreach($val1 as $val) {
-                array_push($resultArr,$val->acl.'_'.$val->module_slug);
+            foreach($userInfo as $user) {
+                array_push($resultArr,$user->acl.'_'.$user->module_slug);
             }
             $userId = Auth::user()->id;
-            $unreadMsgCount=Message::where('to_id',$userId)->where('read_status',0)->count();
+            $unreadMsgCount = Message::where('to_id',$userId)->where('read_status',0)->count();
             Session::put('functionArr',$resultArr);
             return view('admin.dashboard', compact('unreadMsgCount'));
 
