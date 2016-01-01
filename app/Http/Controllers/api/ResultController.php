f003bb267d662e8e670f151ce60be35b184e2a50
Amol Rokade <amol.woxi@gmail.com>
2016-01-01 18:24:37 +0530

    done

--- app/Http/Controllers/api/ResultController.php
+++ app/Http/Controllers/api/ResultController.php
@@ -24,11 +24,11 @@ class ResultController extends Controller
         $studentResultArray=$studentResult->toArray();
         $i=0;
         foreach($studentResultArray as $value){
-            $exam_subject=ExamSubjects::where('id','=',$value['exam_subject_id'])->first();
-            $exam=Exams::where('id','=',$exam_subject['exam_id'])->first();
+            $examSubject=ExamSubjects::where('id','=',$value['exam_subject_id'])->first();
+            $exam=Exams::where('id','=',$examSubject['exam_id'])->first();
             $data[$i]['exam_name']=$exam['exam_name'];
             $data[$i]['marks']=$value['student_marks'];
-            $subject=Subject::where('id','=',$exam_subject['subject_id'])->first();
+            $subject=Subject::where('id','=',$examSubject['subject_id'])->first();
             $data[$i]['subject']=$subject['subject_name'];
             $data[$i]['subject_id']=$subject['id'];
             $data[$i]['exam_id']=$exam['id'];
@@ -36,28 +36,28 @@ class ResultController extends Controller
         }
         $size=count($data);
         $subjectArray = array();
-        for($i=0,$j=0;$i<$size;$i++){
+        for($i=0;$i<$size;$i++){
             $result = array(
                 'subject_id'=> $data[$i]['subject_id'],
                 'subject_name' => $data[$i]['subject'] );
             $subjectArray[$i] = $result;
         }
 
-        $size1=count($subjectArray);
+        $subjectArraysize=count($subjectArray);
         $dataArraySize=count($data);
-        for($i=0;$i<$size1;$i++)
+        for($i=0;$i<$subjectArraysize;$i++)
         {
             $examName=$data[$i]['exam_name'];
             $subjectArraySubject_id=$subjectArray[$i]['subject_id'];
             for($j=0;$j<$dataArraySize;$j++){
                 $dataArraySubject_id= $data[$j]['subject_id'];
                 if($subjectArraySubject_id==$dataArraySubject_id){
-                    $result1 = array(
+                    $result = array(
                            'test_id'=> $data[$i]['exam_id'],
                             'marks'=> $data[$i]['marks'],
                             'subject_id' => $data[$i]['subject_id'],
                             'subject_name' => $data[$i]['subject']);
-                    $finalExamData[$examName][$j]=$result1;
+                    $finalExamData[$examName][$j]=$result;
                 }
             }
         }
