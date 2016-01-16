d4f2a5426d0c56ea5b202d8c1c16938eeddcbf96
ganesh.woxi@gmail.com <ganesh.woxi@gmail.com>
2016-01-16 17:50:50 +0530

    template change

--- resources/views/emails/password.blade.php
+++ resources/views/emails/password.blade.php
@@ -1,9 +1,21 @@
-Hello {{$user->username}},<br/><br/>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<div>
+    Hello {{$user->username}},<br/><br/>
 
-Your Veza account password needs to be changed.<br/>
+    Your Veza account password needs to be changed.<br/>
 
-Please follow these steps to reset your password. <a href="{{ url('password/reset/'.$token) }}"> Reset your password. </a><br/><br/>
+    Please follow these steps to reset your password. <a href="{{ url('password/reset/'.$token) }}"> Reset your password. </a><br/><br/>
 
---The Veza Team.<br/>
+    --The Veza Team.<br/>
 
-This is an automated email, please don’t reply.<br/>
+    This is an automated email, please don’t reply.<br/>
+
+</div>
+
+</body>
+</html>
