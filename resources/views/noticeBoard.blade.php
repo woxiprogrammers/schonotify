f22d97bea490ac7933d9892abfde8de5e117da2f
ganesh.woxi@gmail.com <ganesh.woxi@gmail.com>
2016-01-22 15:28:11 +0530

    create homework issue solved

--- resources/views/noticeBoard.blade.php
+++ resources/views/noticeBoard.blade.php
@@ -15,6 +15,7 @@
     <div class="wrap-content container" id="container">
         <!-- start: DASHBOARD TITLE -->
         @include('alerts.errors')
+        <div id="message-error-div"></div>
         <section id="page-title" class="padding-top-15 padding-bottom-15">
             <div class="row">
                 <div class="col-sm-7">
