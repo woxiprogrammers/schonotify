5205df4c2d0e2090417e4e4cbefa1981156c7a33
suraj.woxi <suraj.woxi@gmail.com>
2016-03-07 17:30:51 +0530

    event view

--- resources/views/admin/event.blade.php
+++ resources/views/admin/event.blade.php
@@ -26,11 +26,32 @@
                 <!-- end: DASHBOARD TITLE -->
                 <div class="container-fluid container-fullw bg-white">
                     <div class="row">
-                        <div class="col-sm-12 space20">
-                            <a href="#newFullEvent" class="btn btn-primary btn-o add-event"><i class="fa fa-plus"></i> Add New Event</a>
+                        <div class="col-sm-2 space20">
+                            <a href="#newFullEvent" class="btn btn-primary btn-o add-event padding-10"><i class="fa fa-plus"></i> Add New Event</a>
                         </div>
+
+                        <select class="col-sm-4 padding-10" id="event-select-dropdown" style="-webkit-appearance: menulist;">
+                            <option value="1" @if($eventSelectionId == 1) selected @endif >All</option>
+                            <option value="2" @if($eventSelectionId == 2) selected @endif>Pending</option>
+                            <option value="3" @if($eventSelectionId == 3) selected @endif>Published</option>
+                        </select>
+                        <div class="col-sm-5 pull-right">
+                            <div class="event-category col-sm-3 event-to-do" >
+                                Draft
+                            </div>
+                            <div class="event-category col-sm-3 event-off-site-work" >
+                                Pending
+                            </div>
+                            <div class="event-category col-sm-3 event-job" >
+                                Published
+                            </div>
+                        </div>
+
                         <div class="col-sm-12">
-                            <div id='full-calendar'></div>
+                            <div id='full-calendar'>
+
+                            </div>
+
                         </div>
 
                     </div>
@@ -39,7 +60,7 @@
                 <div class="modal fade modal-aside horizontal right events-modal"  tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
                     <div class="modal-dialog modal-dialog modal-md">
                         <div class="modal-content">
-                            <form class="form-full-event" id="form">
+                            <form class="form-full-event" id="create_event_form">
                                 <div class="modal-body">
                                     <div id="editEvent">
                                         <div class="form-group ">
@@ -57,7 +78,7 @@
                                             <label>
                                                 Event Details
                                             </label>
-                                            <textarea class="form-control" id="event-name" name="eventDescription"></textarea>
+                                            <textarea class="form-control" id="event-description" name="eventDescription"></textarea>
                                         </div>
                                         <div class="form-group">
                                             <label>
@@ -91,23 +112,38 @@
                                             <div class="panel-heading">
                                                 <div class="timeline_title">
 
-                                                    <h4 class="panel-title no-margin text-primary" id="event-title"></h4>
+                                                    <div class="col-sm-6">
+                                                        <h4 class="panel-title no-margin text-primary" id="event-title"></h4>
+                                                    </div>
+                                                    <div class="panel-tools col-sm-6">
+                                                        <div id="status-show" class="col-sm-4"></div>
+                                                        <div class="col-sm-8"><i class="fa fa-clock-o"></i> <span id="created_time"></span></div>
+                                                    </div>
+                                                </div>
 
-                                                </div>
-                                                <div class="panel-tools">
-                                                    <i class="fa fa-clock-o"></i> Wednesday 12 Nov, 2015 12:00 PM
-                                                </div>
                                             </div>
                                             <div class="panel-body">
-                                                <p id="event-description">
+                                                <div class="col-sm-12">
+                                                    <p id="event-detail">
 
-                                                </p>
-                                                <address>
-                                                    Event Start: <em class="text-bold" id="event-start-time"></em>
+                                                    </p>
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <address>
+                                                        Event Start: <em class="text-bold" id="event-start-time"></em>
+                                                        <br>
+                                                        Event End: <em class="text-bold" id="event-end-time"></em>
+                                                    </address>
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    Created By:<em class="text-bold" id="event-created-by"></em>
                                                     <br>
-                                                    Event End: <em class="text-bold" id="event-end-time"></em>
-                                                </address>
-                                                <img class="thumbnail" src="assets/images/picture.svg" onerror="this.onerror=null;this.width='200';this.src='assets/images/picture.svg'; " width="200">
+                                                    Published By:<em class="text-bold" id="event-published-by"></em>
+                                                </div>
+                                                <div class="col-sm-12">
+                                                    <img class="thumbnail" id="event-image"  width="200">
+                                                </div>
+
                                             </div>
 
                                         </div>
@@ -161,31 +197,32 @@
 
     @include('rightSidebar')
 
-
 </div>
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<script src="vendor/modernizr/modernizr.js"></script>
-<script src="vendor/jquery-cookie/jquery.cookie.js"></script>
-<script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-<script src="vendor/switchery/switchery.min.js"></script>
+
+<div id="loadmoreajaxloader" class="loader-position-event" ><center><img src="/assets/images/loader1.gif" /></center></div>
+
+<script src="/vendor/jquery/jquery.min.js"></script>
+<script src="/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="/vendor/modernizr/modernizr.js"></script>
+<script src="/vendor/jquery-cookie/jquery.cookie.js"></script>
+<script src="/vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<script src="/vendor/switchery/switchery.min.js"></script>
 <!-- end: MAIN JAVASCRIPTS -->
 <!-- start: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
-<script src="vendor/jquery-ui/jquery-ui-1.10.2.custom.min.js"></script>
-<script src="vendor/moment/moment.min.js"></script>
-<script src="vendor/jquery-validation/jquery.validate.min.js"></script>
-<script src="vendor/fullcalendar/fullcalendar.min.js"></script>
-<script src="vendor/bootstrap-datetimepicker/bootstrap-datetimepicker.min.js"></script>
+<script src="/vendor/jquery-ui/jquery-ui-1.10.2.custom.min.js"></script>
+<script src="/vendor/moment/moment.min.js"></script>
+<script src="/vendor/jquery-validation/jquery.validate.min.js"></script>
+<script src="/vendor/fullcalendar/fullcalendar.min.js"></script>
+<script src="/vendor/bootstrap-datetimepicker/bootstrap-datetimepicker.min.js"></script>
 <!-- end: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
 <!-- start: CLIP-TWO JAVASCRIPTS -->
-<script src="assets/js/main.js"></script>
+<script src="/assets/js/main.js"></script>
 <!-- start: JavaScript Event Handlers for this page -->
-<script src="assets/js/pages-calendar.js"></script>
-<script src="assets/js/custom-project.js"></script>
-<script src="vendor/ckeditor/ckeditor.js"></script>
-<script src="vendor/ckeditor/adapters/jquery.js"></script>
-<script src="assets/js/form-validation.js"></script>
-
+<script src="/assets/js/pages-calendar.js"></script>
+<script src="/assets/js/custom-project.js"></script>
+<script src="/vendor/ckeditor/ckeditor.js"></script>
+<script src="/vendor/ckeditor/adapters/jquery.js"></script>
+<script src="/assets/js/form-validation.js"></script>
 
 
 <script>
@@ -193,10 +230,18 @@
         getMsgCount();
         Main.init();
         FormValidator.init();
+
         Calendar.init();
     });
 
+    $('#event-select-dropdown').change(function(){
+        var val=$('#event-select-dropdown').val();
+
+        window.location.href="/event/"+val;
+    });
+
     $('#form').on('submit',function(e){
+
         e.preventDefault();
 
         var file=$(this);
@@ -220,7 +265,6 @@
             error: function(data){
                 // Error...
                 var errors = $.parseJSON(data.responseText);
-
 
                 errorsHtml = '<div class="alert alert-danger"><ul>';
 
