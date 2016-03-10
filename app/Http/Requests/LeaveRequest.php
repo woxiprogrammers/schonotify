f5f10110d8866bf9cea7edc0ff09617ef72c5f29
Amol Rokade <amol.woxi@gmail.com>
2016-03-10 12:43:36 +0530

    Request Leave changed to Leave Request

--- app/Http/Requests/LeaveRequest.php
+++ app/Http/Requests/LeaveRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use App\Http\Requests\Request;
 use App\User;
 
-class Leave extends Request
+class LeaveRequest extends Request
 {
     /**
      * Determine if the user is authorized to make this request.
