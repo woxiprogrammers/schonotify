1e0f221dfe5f78faa367cafd10d8e0373100d7bd
suraj.woxi <suraj.woxi@gmail.com>
2016-04-12 12:30:42 +0530

    textarea issue solved

--- resources/views/composeMessage.blade.php
+++ resources/views/composeMessage.blade.php
@@ -38,7 +38,7 @@
                                     <label class="control-label">
                                         Message
                                     </label>
-                                    <input type="text" placeholder="Enter Text Message Here" class="form-control" name="description"/>
+                                    <textarea class="form-control" placeholder="Enter Text Message Here" name="description"></textarea>
                                 </div>
                             </div>
                         </div>
@@ -73,8 +73,7 @@
                                     <label class="control-label">
                                         Message
                                     </label>
-                                    <textarea class="form-control" placeholder="Enter Text Message Here" name="description">
-                                    </textarea>
+                                    <textarea class="form-control" placeholder="Enter Text Message Here" name="description"></textarea>
                                 </div>
                             </div>
                         </div>
@@ -139,8 +138,7 @@
                                     <label class="control-label">
                                         Message
                                     </label>
-                                    <textarea class="form-control" placeholder="Enter Text Message Here" name="description">
-                                    </textarea>
+                                    <textarea class="form-control" placeholder="Enter Text Message Here" name="description"></textarea>
                                 </div>
                             </div>
                         </div>
