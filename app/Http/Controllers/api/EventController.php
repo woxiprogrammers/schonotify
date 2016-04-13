d412f06aa7fa4fd50572afb74095b28545919b2d
Amol Rokade <amol.woxi@gmail.com>
2016-04-13 17:44:42 +0530

    coment removed

--- app/Http/Controllers/api/EventController.php
+++ app/Http/Controllers/api/EventController.php
@@ -19,7 +19,7 @@ class EventController extends Controller
 
     public function __construct(Request $request)
     {
-       // $this->middleware('db');
+        $this->middleware('db');
         $this->middleware('authenticate.user');
     }
 
