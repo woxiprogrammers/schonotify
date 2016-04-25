51d871d0613967d3b053e19671f29a37d692e203
suraj.woxi <suraj.woxi@gmail.com>
2016-04-25 19:17:52 +0530

    token issue solved

--- app/User.php
+++ app/User.php
@@ -48,4 +48,31 @@ class User extends Model implements AuthenticatableContract,
     public function Message(){
         return $this->hasMany('App\Message','to_id');
     }
+
+    public function getRememberToken()
+    {
+        return null; // not supported
+    }
+
+    public function setRememberToken($value)
+    {
+        // not supported
+    }
+
+    public function getRememberTokenName()
+    {
+        return null; // not supported
+    }
+
+    /**
+     * Overrides the method to ignore the remember token.
+     */
+    public function setAttribute($key, $value)
+    {
+        $isRememberTokenAttribute = $key == $this->getRememberTokenName();
+        if (!$isRememberTokenAttribute)
+        {
+            parent::setAttribute($key, $value);
+        }
+    }
 }
