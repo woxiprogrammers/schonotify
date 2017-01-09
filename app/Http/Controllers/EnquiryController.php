19c2120a6f3aff0a1bd9d4c58475ce0d5dada217
ganesh.woxi@gmail.com <ganesh.woxi@gmail.com>
2017-01-09 15:11:53 +0530

    inital changes done

--- app/Http/Controllers/EnquiryController.php
+++ app/Http/Controllers/EnquiryController.php
@@ -16,7 +16,7 @@ class EnquiryController extends Controller
     public function __construct()
     {
         $this->middleware('db');
-        $this->middleware('auth');
+        //$this->middleware('auth', ['except' => 'viewEnquiryFormWithoutLogin','storeEnquiryFormWithoutLogin']);
 
     }
 
@@ -50,6 +50,45 @@ class EnquiryController extends Controller
             }
             Session::flash('message-success','Student Enquiry Submitted Successfully');
             return redirect('/student-enquiry');
+        }catch(\Exception $e){
+            $data = [
+                'exception' => $e->getMessage()
+            ];
+            Log::critical(json_encode($data));
+            abort(500,$e->getMessage());
+        }
+    }
+
+    public function viewEnquiryFormWithoutLogin(){
+        try{
+            return view('admin.enquiryCreateWithoutLogin');
+        }catch(\Exception $e){
+            $data = [
+
+                'exception' => $e->getMessage()
+            ];
+            Log::critical(json_encode($data));
+            abort(500,$e->getMessage());
+        }
+
+    }
+
+    public function storeEnquiryFormWithoutLogin(Request $request){
+        try{
+            $data = $request->all();
+            $currentTime = Carbon::now();
+            $dob = str_replace('/', '-', $data['dob']);
+            $data['dob'] = date('Y-m-d', strtotime($dob));
+            $data['created_at'] = $currentTime;
+            $data['updated_at'] = $currentTime;
+            $newEnquiry = EnquiryForm::create($data);
+            //return redirect('/new-student-enquiry')->with('message', 'Success!');
+            TCPDF::AddPage();
+            //TCPDF::Write(0, 'Hello World');
+            TCPDF::writeHTML(view('backend.common.pdf.rma-invoice')->with(compact('rmaInfo','sellerAddress','orderInfo'))->render());
+            TCPDF::Output("Invoice".date('Y-m-d_H_i_s').".pdf", 'D');
+            //return view('backend.common.pdf.invoice')->with(compact('order','orderNo','seller','sellerAddress','customerAddress','product','brand','unitPrice','taxPrice','orderDate','sellerBankDetails','invoiceId','invoice','invoiceCreatedDate','orderDateChangeFormat'));
+
         }catch(\Exception $e){
             $data = [
                 'exception' => $e->getMessage()
