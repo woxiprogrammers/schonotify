a8d6f439c8c8a684431e0163a0d609d8202ec55c
sagarwoxi <sagar.woxi@gmail.com>
2017-01-23 11:52:29 +0530

    enquiry form changes

--- resources/views/admin/enquiry/listing.blade.php
+++ resources/views/admin/enquiry/listing.blade.php
@@ -52,28 +52,13 @@
 <div class="main-content" >
 <div class="wrap-content container" id="container">
 <!-- start: PAGE TITLE -->
-<section id="page-title">
-   <!-- <div class="row">
-        <div class="col-sm-8">
-            <h1 class="mainTitle">Advanced Data Tables</h1>
-            <span class="mainDescription">Refers to data arranged in rows and columns. A spreadsheet, for example, is a table. In relational database management systems, all information is stored in the form of tables. <small class="block">Webopedia - Online Tech Dictionary for IT Professionals</small></span>
-        </div>
-        <ol class="breadcrumb">
-            <li>
-                <span>Tables</span>
-            </li>
-            <li class="active">
-                <span>Data Tables</span>
-            </li>
-        </ol>
-    </div>-->
-</section>
+
 <!-- end: PAGE TITLE -->
 <!-- start: DYNAMIC TABLE -->
 <div class="container-fluid container-fullw bg-white">
 <div class="row">
 <div class="col-md-12">
-<h5 class="over-title margin-bottom-15">Basic <span class="text-bold">Data Table</span></h5>
+<h5 class="over-title margin-bottom-15"><h2>Enquiry Listing</h2></h5>
 
 <table class="table table-striped table-bordered table-hover table-full-width" id="sample_1">
 <thead>
