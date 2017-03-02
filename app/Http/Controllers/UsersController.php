08334fd4c33e8a3718d2db65b8a25d5a21a61fd0
sagarwoxi <sagar.woxi@gmail.com>
2017-03-02 19:29:17 +0530

    issues resolved

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -873,9 +873,8 @@ class UsersController extends Controller
                    }
                    foreach($installment_percent_amount as $amount)
                    {
-                     //For history
+
                    }
-                $concession_amount_array=array();
                 $caste_concession_type=StudentFee::where('student_id',$id)->pluck('caste_concession');
                 $caste_concn_amnt= CASTECONCESSION::where('caste_id', $caste_concession_type)->where('fee_id',$feedata)->pluck('concession_amount');
                 $collection=collect($installment_percent_amount);
@@ -886,14 +885,13 @@ class UsersController extends Controller
                        $concession_amount_array[$key] = $discounted_amount_for_installment;
                    }
                 $final_discounted_amounts = array();
-
-                if(count($concession_amount_array) == count($total_installment_amount))
+                /*if(count($concession_amount_array) == count($total_installment_amount))
                 {
                     foreach($concession_amount_array as $key => $value)
                     {
                         $final_discounted_amounts[$key] = $total_installment_amount[$key] - $value;
                     }
-                }
+                }*/
                 $final_fee_details=array_combine($final_discounted_amounts,$fee_due_date);
                 $fee_pert=fee_particulars::select('particular_name')->get()->toArray();
                 if(!empty($installment_info))
