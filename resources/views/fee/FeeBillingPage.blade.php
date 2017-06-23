8fe50a454505ffa83cdba02c1848f1f92d3512e7
bharatwoxi <bharat.woxi@gmail.com>
2017-06-23 09:00:16 +0000

    changes done

--- resources/views/fee/FeeBillingPage.blade.php
+++ resources/views/fee/FeeBillingPage.blade.php
@@ -70,7 +70,7 @@
 <script src="/vendor/jquery-modal/jquery.modal.js"></script>
 <script src="/vendor/bootstrap/js/bootstrap.min.js"></script>
 <script src="/vendor/modernizr/modernizr.js"></script>
-<script src="/vendor/jquery-cookie/jquery.cookie.js"></script>4a
+<script src="/vendor/jquery-cookie/jquery.cookie.js"></script>
 <script src="/vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
 <script src="/vendor/switchery/switchery.min.js"></script>
 <!-- end: MAIN JAVASCRIPTS -->
