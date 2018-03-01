e11ba5b934c0f845d4796be6fa9dab233f24b6a3
Nishank <nishank.woxi@gmail.com>
2018-03-01 18:47:13 +0530

    folder name create completed

---
+++ resources/views/gallery/folderView.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amol
+ * Date: 2/28/18
+ * Time: 3:13 PM
+ */
---
+++ resources/views/gallery/galleryManagementView.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amol
+ * Date: 3/1/18
+ * Time: 10:49 AM
+ */
