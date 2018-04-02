5530ede3dd814517b5e45462af1f391bcce27ab3
Nishank <nishank.woxi@gmail.com>
2018-04-02 10:59:26 +0530

    Late fee create and view Done

---
+++ database/migrations/2018_03_29_165108_add_late_fee_amount_to_fee_due_date_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLateFeeAmountToFeeDueDateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('fee_due_date', function (Blueprint $table) {
+            $value=0;
+            $table->integer('late_fee_amount')->default($value);
+            $table->integer('number_of_days')->default($value);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('fee_due_date', function (Blueprint $table) {
+            $table->dropColumn('late_fee_amount');
+            $table->dropColumn('number_of_days');
+        });
+    }
+}
