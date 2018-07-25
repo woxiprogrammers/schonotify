41248feb75cfc35bb93b9dca6414bde3c0cf7061
Nishank <nishank.woxi@gmail.com>
2018-07-25 15:16:59 +0530

    parent login restricted if student is not active

--- app/Http/Controllers/api/MessageController.php
+++ app/Http/Controllers/api/MessageController.php
@@ -388,7 +388,7 @@ class MessageController extends Controller
         try{
             $data=$requests->all();
             $teacher_id = UserRoles::whereIn('slug', ['teacher'])->pluck('id');
-            $teacher = User::where('role_id',$teacher_id)->where('body_id',$data['teacher']['body_id'])->get()->toArray();
+            $teacher = User::where('role_id',$teacher_id)->where('body_id',$data['teacher']['body_id'])->where('is_active',1)->get()->toArray();
             $i=0;
             foreach($teacher as $value){
                 if($value['id']!=$data['teacher']['id'])
