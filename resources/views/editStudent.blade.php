f45d8d483de556f20f1d25e7e1a729b98b1287a6
Nishank <nishank.woxi@gmail.com>
2018-08-07 10:45:00 +0530

    modal header name changed

--- resources/views/editStudent.blade.php
+++ resources/views/editStudent.blade.php
@@ -1202,7 +1202,7 @@
                                             <div class="modal-content">
                                                 <div class="modal-header">
                                                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                                    <h4 class="modal-title">Modal Header</h4>
+                                                    <h4 class="modal-title">Shuffle Student</h4>
                                                 </div>
                                                 <div class="modal-body">
                                                     <p>Are You sure you want to shuffle the student <b>{{$user->first_name}} {{$user->middle_name}} {{$user->last_name}}</b></p>
